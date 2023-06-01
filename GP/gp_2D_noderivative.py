68bd542377491687f7e7818b145d930f38e39d27
Kenta Ogawa <gakenshizu1199@gmail.com>
2023-06-01 14:48:33 +0900

    add nonderivateve gps(naive)

---
+++ GP/gp_2D_noderivative.py
@@ -0,0 +1,120 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax import grad, jit, vmap
+
+from stopro.GP.gp import GPmodel
+from stopro.GP.kernels import define_kernel
+
+
+class GP2DNoDerivative(GPmodel):
+    def __init__(self, model_param):
+        kernel_type = model_param["kernel_type"]
+        kernel_form = model_param["kernel_form"]
+        distance_func = model_param["distance_func"]
+
+        def outermap(f):
+            return vmap(vmap(f, in_axes=(None, 0, None)), in_axes=(0, None, None))
+
+    def setup_training_and_predicting_functions(self, kernel_type, kernel_form):
+        def outermap(f):
+            return vmap(vmap(f, in_axes=(None, 0, None)), in_axes=(0, None, None))
+
+        Kernel = define_kernel(kernel_type, kernel_form, input_dim=2)
+        K = outermap(Kernel)
+
+        def trainingK_all(θ, train_pts):
+            """
+            Args :
+              θ  : kernel hyperparameters
+             args: training points r_ux,r_uy,r_p,r_fx,r_fy,r_div
+            """
+            θyy = θ
+
+            def Kyy(r, rp):
+                return K(r, rp, θyy)
+
+            Ks = [
+                [Kyy],
+            ]
+
+            return self.calculate_K_symmetric(train_pts, Ks)
+
+        def mixedK_all(θ, test_pts, train_pts):
+            θyy = θ
+
+            def Kyy(r, rp):
+                return K(r, rp, θyy)
+
+            Ks = [[Kyy]]
+            return self.calculate_K_asymmetric(train_pts, test_pts, Ks)
+
+        def testK_all(θ, r_test):
+            θyy = θ
+
+            def Kyy(r, rp):
+                return K(r, rp, θyy)
+
+            Ks = [[Kyy]]
+            return self.calculate_K_symmetric(r_test, Ks)
+
+        def trainingFunction_all(θ, *args):
+            """Returns minus log-likelihood given Kernel hyperparamters θ and training data args
+            args = velocity position, velocity average, velocity values,
+                   force position, force average, force values,
+                   jiggle parameter
+            """
+            # r,μ,f,ϵ=args
+            r, μ, f, ϵ = args
+            r_num = len(r)
+            for i in range(r_num):
+                if i == 0:
+                    δy = jnp.array(f[i] - μ[i])
+                else:
+                    δy = jnp.concatenate([δy, f[i] - μ[i]], 0)
+            Σ = trainingK_all(θ, r)
+            return self.logpGP(δy, Σ, ϵ)
+
+        def predictingFunction_all(θ, *args):
+            """Returns conditional posterior average and covariance matrix given Kernel hyperparamters θ  and test and training data
+            args = test velocity position, test velocity average,
+                   training velocity position, training velocity average, training velocity values
+                   training force position, training force average, training force values
+                   jiggle parameter
+
+            Returns
+            -----------------
+            μpost=[μux,μuy,μp]
+            Σpost=[Σux,Σuy,Σp]
+            """
+            r_test, μ_test, r_train, μ, f_train, ϵ = args
+            nb = 0
+            for r in r_train:
+                nb += len(r)
+            Σbb = trainingK_all(θ, r_train)
+            Σab = mixedK_all(θ, r_test, r_train)
+            Σaa = testK_all(θ, r_test)
+            for i in range(len(r_train)):
+                if i == 0:
+                    δfb = jnp.array(f_train[i] - μ[i])
+                else:
+                    δfb = jnp.concatenate([δfb, f_train[i] - μ[i]])
+                    # create single training array, with velocities and forces (second derivatives)
+            #         print(f'δy={δy}')
+            #         print(f'Σ={Σ}')
+            μposts, Σposts = self.postGP(δfb, Σaa, Σab, Σbb, ϵ)
+            # seperate μpost,Σpost to 3 section (ux,uy,p)
+            sec0 = 0
+            sec1 = 0
+            μpost = []
+            Σpost = []
+            for i in range(len(r_test)):
+                sec1 += len(r_test[i])
+                μpost.append(μposts[sec0:sec1])
+                Σpost.append(Σposts[sec0:sec1, sec0:sec1])
+                sec0 += len(r_test[i])
+                # 一応解決ちょっと疑問残る
+                μpost[i] += μ_test[i]
+            return μpost, Σpost
+
+        return jit(trainingFunction_all), jit(predictingFunction_all)
--- solver/optimizers.py
+++ solver/optimizers.py
@@ -4,57 +4,108 @@ from jax import jit, value_and_grad
 from jax.example_libraries import optimizers
 from scipy import optimize
 
+NO_GRAD_OPTIMIZE_LIST = [
+    "Nelder-Mead",
+    "Powell",
+]
+
+FIRST_GRAD_OPTIMIZE_LIST = [
+    "TNC",
+    "BFGS",
+    "L-BFGS-B",  # ?
+]
+SECOND_GRAD_OPTIMIZE_LIST = [
+    "CG",
+]
+
+
+def optimize_with_scipy(func, dfunc, hess, res, theta, optimize_param, args):
+    method_first = optimize_param["method_first"]
+    maxiter_first = optimize_param["maxiter_first"]
+
+    opt = {"maxiter": maxiter_first, "disp": 0}
+
+    if method_first in NO_GRAD_OPTIMIZE_LIST:
+        dfunc = None
+        hess = None
+    elif method_first in FIRST_GRAD_OPTIMIZE_LIST:
+        hess = None
+    elif method_first in SECOND_GRAD_OPTIMIZE_LIST:
+        pass
+    else:
+        raise Exception("scipy optimize method is not in list")
+    res.append(
+        optimize.minimize(
+            func,
+            res[-1]["x"],
+            args=args,
+            method=method_first,
+            jac=dfunc,
+            hess=hess,
+            options=opt,
+        )
+    )
+    print(
+        f"loss after first optimize: {res[-1]['fun']}, \n result of first optimize{res}"
+    )
+    theta.append(res[-1]["x"])
+    return res, theta
+
 
 def optimize_by_adam(f, df, hf, init, optimize_param, *args):
-    maxiter_GD = optimize_param['maxiter_GD']
-    lr = optimize_param['lr']
-    eps = optimize_param['eps']
-    maxiter_first = optimize_param['maxiter_first']
-    method_GD = optimize_param['method_GD']
-    method_first = optimize_param['method_first']
+    maxiter_GD = optimize_param["maxiter_GD"]
+    lr = optimize_param["lr"]
+    eps = optimize_param["eps"]
+    maxiter_first = optimize_param["maxiter_first"]
+    method_GD = optimize_param["method_GD"]
+    method_first = optimize_param["method_first"]
     print(method_first)
     loss = []
     theta = [init]
     norm_of_grads_list = []
 
     def printer(loss, opt_result, init):
-        print(f'\t loss = {loss[-1]:12.6e}, niter = {maxiter_GD:5d}')
-        print(f'\t\t     θ0 = {jnp.exp(init)}')
-        print(f'\t\t      θ = {jnp.exp(opt_result)}')
-        print(f'\t\t log(θ) = {opt_result}')
+        print(f"\t loss = {loss[-1]:12.6e}, niter = {maxiter_GD:5d}")
+        print(f"\t\t     θ0 = {jnp.exp(init)}")
+        print(f"\t\t      θ = {jnp.exp(opt_result)}")
+        print(f"\t\t log(θ) = {opt_result}")
 
     # _:ignore returns
     r_train, *_ = args
     ntraining = 0
-    opt = {'maxiter': maxiter_first, 'disp': 0}
-    res = [{'x': init}]
+    # opt = {"maxiter": maxiter_first, "disp": 0}
+    res = [{"x": init}]
     for r in r_train:
         ntraining += r.shape[0]
     # training pointsのかず
-    func = lambda p, *args: f(p, *args)/ntraining
-    dfunc = lambda p, *args: df(p, *args)/ntraining
-    hess = lambda p, *args: hf(p, *args)/ntraining
+    func = lambda p, *args: f(p, *args) / ntraining
+    dfunc = lambda p, *args: df(p, *args) / ntraining
+    hess = lambda p, *args: hf(p, *args) / ntraining
 
     def step(t, opt_state, optimizer, theta):
-        value, grads = value_and_grad(func)(
-            theta, *args)
+        value, grads = value_and_grad(func)(theta, *args)
         updates, opt_state = optimizer.update(grads, opt_state, theta)
         theta = optax.apply_updates(theta, updates)
         norm_of_grads = jnp.sqrt(jnp.sum(jnp.square(grads)))
         print(
-            f'step{t:4} loss: {value:.4f} max_grad: {jnp.max(abs(grads)):.5f}, arg={jnp.argmax(abs(grads))}')
-        print(f'norm_of_grads: {norm_of_grads:.5f}')
-        for thet in jnp.split(theta[:18], 6):
-            print(f'{jnp.round(thet, 4)}')
-        print(f'theta_max: {jnp.max(theta):.5f}')
-        print(f'theta_min: {jnp.min(theta):.5f}\n')
+            f"step{t:4} loss: {value:.4f} max_grad: {jnp.max(abs(grads)):.5f}, arg={jnp.argmax(abs(grads))}"
+        )
+        print(f"norm_of_grads: {norm_of_grads:.5f}")
+        if len(theta) >= 18:
+            for thet in jnp.split(theta[:18], 6):
+                print(f"{jnp.round(thet, 4)}")
+        else:
+            print(jnp.round(theta, 4))
+        print(f"theta_max: {jnp.max(theta):.5f}")
+        print(f"theta_min: {jnp.min(theta):.5f}\n")
         return value, opt_state, theta, norm_of_grads
 
     def doopt(init, optimizer, maxiter_GD, loss, theta, norm_of_grads_list):
         opt_state = optimizer.init(init)
         for t in range(maxiter_GD):
             value, opt_state, current_theta, norm_of_grads = step(
-                t, opt_state, optimizer, theta[-1])
+                t, opt_state, optimizer, theta[-1]
+            )
             # keep norm of gradient, theta and loss in lists
             norm_of_grads_list.append(norm_of_grads)
             theta.append(current_theta)
@@ -66,81 +117,79 @@ def optimize_by_adam(f, df, hf, init, optimize_param, *args):
             #     break
             # judge convergence by norm of gradient
             elif norm_of_grads < eps:
-                print('converged')
+                print("converged")
                 break
             elif jnp.any(jnp.isnan(theta[-1])):
                 print("diverged")
                 raise Exception("発散しました、やりなおしましょう")
         return theta, loss
+
     # optimize by scipy method
     loss_before_optimize = func(init, *args)
-    print(f'loss before optimize: {loss_before_optimize}')
+    print(f"loss before optimize: {loss_before_optimize}")
     loss.append(loss_before_optimize)
     if jnp.isnan(loss_before_optimize):
         raise Exception("初期条件でlossがnanになりました")
+    # optimize by scipy
     if maxiter_first:
-        if method_first == 'Nelder-Mead' or method_first == 'L-BFGS-B':
-            res.append(optimize.minimize(
-                func, res[-1]['x'], args=args, method=method_first, options=opt))
-            print(
-                f"loss after first optimize: {res[-1]['fun']}, \n result of first optimize{res}")
-        elif method_first == 'TNC' or method_first == 'BFGS':
-            res.append(optimize.minimize(
-                func, res[-1]['x'], args=args, method=method_first, jac=dfunc, options=opt))
-        theta.append(res[-1]['x'])
+        res, theta = optimize_with_scipy(
+            func, dfunc, hess, res, theta, optimize_param, args
+        )
     # optimize by sgd
-    if method_GD == 'adam':
+    if method_GD == "adam":
         optimizer = optax.adam(lr)
-    elif method_GD == 'sgd':
+    elif method_GD == "sgd":
         opt_init, opt_update, get_params = optimizers.sgd(lr)
 
     if maxiter_GD:
         theta, loss = doopt(
-            res[-1]['x'], optimizer, maxiter_GD, loss, theta, norm_of_grads_list)
+            res[-1]["x"], optimizer, maxiter_GD, loss, theta, norm_of_grads_list
+        )
 
     return theta[-1], loss, theta, norm_of_grads_list
 
 
 def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
-    maxiter_GD = optimize_param['maxiter_GD']
-    lr = optimize_param['lr']
-    eps = optimize_param['eps']
-    maxiter_first = optimize_param['maxiter_first']
-    method_GD = optimize_param['method_GD']
-    method_first = optimize_param['method_first']
+    maxiter_GD = optimize_param["maxiter_GD"]
+    lr = optimize_param["lr"]
+    eps = optimize_param["eps"]
+    maxiter_first = optimize_param["maxiter_first"]
+    method_GD = optimize_param["method_GD"]
+    method_first = optimize_param["method_first"]
     print(method_first)
     loss = []
     theta = [init]
     norm_of_grads_list = []
 
     def printer(loss, opt_result, init):
-        print(f'\t loss = {loss[-1]:12.6e}, niter = {maxiter_GD:5d}')
-        print(f'\t\t     θ0 = {jnp.exp(init)}')
-        print(f'\t\t      θ = {jnp.exp(opt_result)}')
-        print(f'\t\t log(θ) = {opt_result}')
+        print(f"\t loss = {loss[-1]:12.6e}, niter = {maxiter_GD:5d}")
+        print(f"\t\t     θ0 = {jnp.exp(init)}")
+        print(f"\t\t      θ = {jnp.exp(opt_result)}")
+        print(f"\t\t log(θ) = {opt_result}")
 
     # _:ignore returns
     r_train, *_ = args
     ntraining = 0
-    opt = {'maxiter': maxiter_first, 'disp': 0}
-    res = [{'x': init}]
+    opt = {"maxiter": maxiter_first, "disp": 0}
+    res = [{"x": init}]
     for r in r_train:
         ntraining += r.shape[0]
     # training pointsのかず
-    func = lambda p, *args: f(p, *args)/ntraining
-    dfunc = lambda p, *args: df(p, *args)/ntraining
-    hess = lambda p, *args: hf(p, *args)/ntraining
+    func = lambda p, *args: f(p, *args) / ntraining
+    dfunc = lambda p, *args: df(p, *args) / ntraining
+    hess = lambda p, *args: hf(p, *args) / ntraining
 
     def step(t, opt_state, diverged, term_for_regularize_grads, theta_before):
         value, grads = value_and_grad(func)(get_params(opt_state), *args)
         norm_of_grads = jnp.sqrt(jnp.sum(jnp.square(grads)))
         print(
-            f'step{t:4} loss: {value:.3e} max_grad: {jnp.max(abs(grads)):.3e}, arg={jnp.argmax(abs(grads))}')
+            f"step{t:4} loss: {value:.3e} max_grad: {jnp.max(abs(grads)):.3e}, arg={jnp.argmax(abs(grads))}"
+        )
         # 更新したthetaの値でlossが計算不可だった場合は、gradsに倍率(term~)をかけて小さくして更新したthetaを用いる。この倍率は、場合によっては最大1まで戻され、発散する場合はどんどん小さくなる
         grads *= term_for_regularize_grads  # gradsを補正
         # 学習が遅くなりすぎないように、もし前回発散していない、かつtermが0.1以下であれば10倍する
         if not diverged and term_for_regularize_grads <= 0.1:
-            term_for_regularize_grads *= 10.
+            term_for_regularize_grads *= 10.0
         diverged = False
         for _ in range(11):
             opt_state = opt_init(theta_before)
@@ -149,7 +198,7 @@ def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
             # if jnp.any(jnp.isnan(theta)):
             # もし更新したthetaにおけるlossが計算できなければ、gradsを小さくしてthetaの更新をやり直す
             if jnp.isnan(func(theta, *args)):
-                print('isnan')
+                print("isnan")
                 term_for_regularize_grads *= 0.01
                 grads *= 0.01
                 diverged = True
@@ -157,7 +206,8 @@ def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
                 # 次回のlossが収束していれば、ループを抜ける
                 break
         print(
-            f'norm_of_grads: {norm_of_grads:.3e}, term_for_regularize_grads:{term_for_regularize_grads:.2e}\n')
+            f"norm_of_grads: {norm_of_grads:.3e}, term_for_regularize_grads:{term_for_regularize_grads:.2e}\n"
+        )
         # for thet in jnp.split(theta, 6):
         #     print(f'{thet}')
         # print(f'theta_max: {jnp.max(theta):.5f}')
@@ -167,10 +217,11 @@ def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
     def doopt(init, opt_init, get_params, maxiter_GD, loss, theta, norm_of_grads_list):
         opt_state = opt_init(init)
         diverged = False
-        term_for_regularize_grads = 1.
+        term_for_regularize_grads = 1.0
         for t in range(maxiter_GD):
             value, opt_state, norm_of_grads, diverged, term_for_regularize_grads = step(
-                t, opt_state, diverged, term_for_regularize_grads, theta[-1])
+                t, opt_state, diverged, term_for_regularize_grads, theta[-1]
+            )
             # keep norm of gradient, theta and loss in lists
             norm_of_grads_list.append(norm_of_grads)
             theta.append(get_params(opt_state))
@@ -182,39 +233,60 @@ def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
             #     break
             # judge convergence by norm of gradient
             elif norm_of_grads < eps:
-                print('converged')
+                print("converged")
                 break
             elif jnp.any(jnp.isnan(theta[-1])):
                 print("diverged")
                 raise Exception("発散しました、やりなおしましょう")
         return theta, loss
+
     # optimize by scipy method
     loss_before_optimize = func(init, *args)
-    print(f'loss before optimize: {loss_before_optimize}')
+    print(f"loss before optimize: {loss_before_optimize}")
     loss.append(loss_before_optimize)
     if jnp.isnan(loss_before_optimize):
         raise Exception("初期条件でlossがnanになりました")
     if maxiter_first:
-        if method_first == 'Nelder-Mead' or method_first == 'L-BFGS-B':
-            res.append(optimize.minimize(
-                func, res[-1]['x'], args=args, method=method_first, options=opt))
+        if method_first == "Nelder-Mead" or method_first == "L-BFGS-B":
+            res.append(
+                optimize.minimize(
+                    func, res[-1]["x"], args=args, method=method_first, options=opt
+                )
+            )
             print(
-                f"loss after first optimize: {res[-1]['fun']}, \n result of first optimize{res}")
-        elif method_first == 'TNC' or method_first == 'BFGS':
-            res.append(optimize.minimize(
-                func, res[-1]['x'], args=args, method=method_first, jac=dfunc, options=opt))
-        theta.append(res[-1]['x'])
+                f"loss after first optimize: {res[-1]['fun']}, \n result of first optimize{res}"
+            )
+        elif method_first == "TNC" or method_first == "BFGS":
+            res.append(
+                optimize.minimize(
+                    func,
+                    res[-1]["x"],
+                    args=args,
+                    method=method_first,
+                    jac=dfunc,
+                    options=opt,
+                )
+            )
+        theta.append(res[-1]["x"])
     # optimize by sgd
-    if method_GD == 'adam':
+    if method_GD == "adam":
         opt_init, opt_update, get_params = optimizers.adam(lr)
-    elif method_GD == 'sgd':
+    elif method_GD == "sgd":
         opt_init, opt_update, get_params = optimizers.sgd(lr)
 
     if maxiter_GD:
         theta, loss = doopt(
-            res[-1]['x'], opt_init, get_params, maxiter_GD, loss, theta, norm_of_grads_list)
+            res[-1]["x"],
+            opt_init,
+            get_params,
+            maxiter_GD,
+            loss,
+            theta,
+            norm_of_grads_list,
+        )
 
     return theta[-1], loss, theta, norm_of_grads_list
+
 
 # fを初期条件initで各最適化方法で最適化する
 
@@ -222,27 +294,31 @@ def optimize_by_adam_avoid_divergence(f, df, hf, init, optimize_param, *args):
 def optimizeParameters(f, df, hf, init, *args):
     def printer(o):
         print(
-            f'\t loss = {o["fun"]:12.6e}, niter = {o["nit"]:5d}, Converged = {o["success"]:6b} : {o["message"]}')
-        print(f'\t\t     θ0 = {jnp.exp(init)}')
+            f'\t loss = {o["fun"]:12.6e}, niter = {o["nit"]:5d}, Converged = {o["success"]:6b} : {o["message"]}'
+        )
+        print(f"\t\t     θ0 = {jnp.exp(init)}")
         print(f'\t\t      θ = {jnp.exp(o["x"])}')
         print(f'\t\t log(θ) = {o["x"]}')
 
     # maxiter:最大試行回数
     # disp:convergenceを出力するか否か
-    opt = {'maxiter': 200, 'disp': True}
-    res = [{'x': init}]
+    opt = {"maxiter": 200, "disp": True}
+    res = [{"x": init}]
     # _:ignore returns
     r_train, *_ = args
     ntraining = 0
     for r in r_train:
         ntraining += r.shape[0]
     # training pointsのかず
-    func = lambda p, *args: f(p, *args)/ntraining
-    dfunc = lambda p, *args: df(p, *args)/ntraining
-    hess = lambda p, *args: hf(p, *args)/ntraining
+    func = lambda p, *args: f(p, *args) / ntraining
+    dfunc = lambda p, *args: df(p, *args) / ntraining
+    hess = lambda p, *args: hf(p, *args) / ntraining
     # 初期値を:res[-1]['x']とすることで、前回得られたhyperparameterを次回最適化の初期値として用いている
     # func(init,args)となるように定義しておく
-    res.append(optimize.minimize(
-        func, res[-1]['x'], args=args, method='Nelder-Mead', options=opt))
+    res.append(
+        optimize.minimize(
+            func, res[-1]["x"], args=args, method="Nelder-Mead", options=opt
+        )
+    )
     printer(res[-1])
     return res
